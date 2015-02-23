5a775fe1675dfb5cafd0274d81749924a84b8076
Tom Hartley <tomhartley@rocketmail.com>
2015-02-23 21:13:53 +0000

    Improved code for lasers

--- laser_detection/laser_detection.ino
+++ laser_detection/laser_detection.ino
@@ -2,16 +2,18 @@ void setup()
 {
 Serial.begin(115200);
 pinMode(6,OUTPUT);
+digitalWrite(6,LOW);
 }
 
 float read_ldr(int pin_number)
 {
+ //Serial.println(analogRead(pin_number));
  return analogRead(pin_number); 
 }
 
-bool laser_on(int pin_number, int threshold)
+boolean laser_on(int pin_number, int threshold)
 {
-  if(analogRead(pin_number) > threshold)
+  if(read_ldr(pin_number) < threshold)
   {
     return true;
   }
@@ -21,18 +23,40 @@ bool laser_on(int pin_number, int threshold)
 void loop()
 {
   float time = 0;
-  time = random(10,1000);//wait random amount of time
+  
+  boolean a = laser_on(5,600);
+  
+  time = random(50,200);//wait random amount of time
+  //Serial.println("TURNING LASER ON");
   digitalWrite(6,HIGH);//turn laser on!!
-  if(laser_on(5,150)) Serial.println("All is Well");
-  else Serial.println("ALERT!!");  
+  delay(10);
+
+  boolean b = laser_on(5,600);
+  
+  if (!a&&b) {
+    Serial.println("All is Well");
+  } else {
+    Serial.println("ALERT");
+  }
+    
   delay(time);//waigt random amount of time
+  
+  a = laser_on(5,600);
+  //Serial.println("TURNING LASER OFF");
   digitalWrite(6,LOW);//turn laser off!
-  delay(0.01);
-  if(!laser_on(5,150)) Serial.println("All is Well");
-  else Serial.println("ALERT!!");  
-  time = random(10,1000);//wait random amount of time
+  delay(50);
+  
+  b = laser_on(5,600);
+  
+  if (a&&!b) {
+    Serial.println("All is Well");
+  } else {
+    Serial.println("ALERT");
+  }
+
+  time = random(50,200);//wait random amount of time
+  
   delay(time);
-  if(!laser_on(5,150)) Serial.println("All is Well");
-  else Serial.println("ALERT!!");  
+  
 }
 
